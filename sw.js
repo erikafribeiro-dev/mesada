1a2c5a98feaeb3408b5339afc6e48ddd92f37cca
erikafribeiro-dev <erikafribeiro@gmail.com>
2026-09-04 21:14:37 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mesada-leo-v18';
+const CACHE = 'mesada-leo-v19';
 const ARQUIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
